7ac59713eb8af01d2497617474bcc796f9b8d2b1
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-14 18:34:31 -0300

    criando Model Mapper

---
+++ src/main/java/br/com/banco/config/MapperConfig.java
@@ -0,0 +1,16 @@
+package br.com.banco.config;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+
+@Bean
+public ModelMapper mapper(){
+    return new ModelMapper();
+}
+    
+}
