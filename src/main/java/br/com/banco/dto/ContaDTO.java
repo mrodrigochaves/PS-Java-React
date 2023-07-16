d2efe9cf9be0bb2df514559888e4dd8f641a3ff0
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-16 10:41:58 -0300

    refatorando dto de conta

--- src/main/java/br/com/banco/dto/ContaDTO.java
+++ src/main/java/br/com/banco/dto/ContaDTO.java
@@ -1,12 +1,21 @@
 package br.com.banco.dto;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 import lombok.NonNull;
 
 @Data
 public class ContaDTO {
-    
+
     @NonNull
+    private Long id;
+
+    @NotBlank
     private String nomeResponsavel;
+
+    public ContaDTO() {
+
+    }
 
 }
