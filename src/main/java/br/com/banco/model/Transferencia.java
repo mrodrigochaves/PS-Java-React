3353c45e60038cffc660bd72c47105d4bd872eca
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-16 10:45:12 -0300

    configurando retorno de transferencias

--- src/main/java/br/com/banco/model/Transferencia.java
+++ src/main/java/br/com/banco/model/Transferencia.java
@@ -1,19 +1,21 @@
 package br.com.banco.model;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import lombok.NonNull;
+import lombok.Data;
+
 
 @Entity
+@Data
 public class Transferencia {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,7 +26,7 @@ public class Transferencia {
     private LocalDateTime dataTransferencia;
 
     @Column(name="valor", nullable = false)
-    private double valor;
+    private BigDecimal valor;
 
     @Column(name="tipo", nullable = false)
     private String tipo;
@@ -32,41 +34,39 @@ public class Transferencia {
     @Column(name = "nome_operador_transacao")
     private String nomeOperadorTransacao;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "conta_id", nullable = false)
     private Conta conta;
 
     public Transferencia() {
     }
 
-    public Transferencia(Long id, LocalDateTime dataTransferencia, double valor, String tipo,
-            String nomeOperadorTransacao, Conta conta) {
-        this.id = id;
+    public Transferencia(LocalDateTime dataTransferencia, String tipo, String nomeOperadorTransacao, Conta conta, BigDecimal valor) {
         this.dataTransferencia = dataTransferencia;
-        this.valor = valor;
         this.tipo = tipo;
         this.nomeOperadorTransacao = nomeOperadorTransacao;
         this.conta = conta;
-    }
-
-    public void setDataTransferencia(@NonNull LocalDateTime dataTransferencia) {
-        this.dataTransferencia = dataTransferencia;
-    }
-
-    public void setValor( double valor) {
         this.valor = valor;
+    }
+
+    public void setDataTransferencia(LocalDateTime dataTransferencia) {
+        this.dataTransferencia = dataTransferencia;
     }
 
     public void setTipo( String tipo) {
         this.tipo = tipo;
     }
 
-    public void setNomeOperadorTransacao(@NonNull String nomeOperadorTransacao) {
+    public void setNomeOperadorTransacao(String nomeOperadorTransacao) {
         this.nomeOperadorTransacao = nomeOperadorTransacao;
     }
 
     public void setConta(Conta conta) {
         this.conta = conta;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
     }
 
 }
