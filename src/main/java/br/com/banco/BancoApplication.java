456e36676b94f8489101340e58d7a9caa9cf1e50
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-16 10:40:26 -0300

    atualizando application

--- src/main/java/br/com/banco/BancoApplication.java
+++ src/main/java/br/com/banco/BancoApplication.java
@@ -1,41 +1,65 @@
 package br.com.banco;
 
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
 
 import br.com.banco.model.Conta;
 import br.com.banco.model.Transferencia;
 import br.com.banco.repository.ContaRepository;
 import br.com.banco.repository.TransferenciaRepository;
-import java.time.LocalDateTime;
+
+
 
 @SpringBootApplication
-public class BancoApplication {
+public class BancoApplication implements CommandLineRunner {
+    private final ContaRepository contaRepository;
+    private final TransferenciaRepository transferenciaRepository;
+
+    public BancoApplication(ContaRepository contaRepository, TransferenciaRepository transferenciaRepository) {
+        this.contaRepository = contaRepository;
+        this.transferenciaRepository = transferenciaRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BancoApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner init(ContaRepository contaRepository, TransferenciaRepository transferenciaRepository) {
-        return args -> {
-            Conta conta = new Conta();
-            conta.setNomeResponsavel("João da Silva");
-            contaRepository.save(conta);
+    @Override
+    public void run(String... args) {
+      
+        Conta conta1 = new Conta();
+        conta1.setNomeResponsavel("Fulano");
+        contaRepository.save(conta1);
 
+        Conta conta2 = new Conta();
+        conta2.setNomeResponsavel("Sicrano");
+        contaRepository.save(conta2);
 
-            Transferencia transferencia = new Transferencia();
-            transferencia.setDataTransferencia(LocalDateTime.now());
-            transferencia.setValor(100.00);
-            transferencia.setTipo("DEPOSITO");
-            transferencia.setNomeOperadorTransacao("Maria da Silva");
-            transferencia.setConta(conta);
-            transferenciaRepository.save(transferencia);
+      
+        Transferencia transferencia1 = new Transferencia();
+        transferencia1.setDataTransferencia(LocalDateTime.of(2023, 1, 1, 12, 0));
+        transferencia1.setValor(new BigDecimal("30895.46"));
+        transferencia1.setTipo("DEPOSITO");
+        transferencia1.setConta(conta1);
+        transferenciaRepository.save(transferencia1);
 
-        };
+        Transferencia transferencia2 = new Transferencia();
+        transferencia2.setDataTransferencia(LocalDateTime.of(2023, 2, 3, 9, 53, 27));
+        transferencia2.setValor(new BigDecimal("12.24"));
+        transferencia2.setTipo("DEPOSITO");
+        transferencia2.setConta(conta2);
+        transferenciaRepository.save(transferencia2);
+
+       
+        System.out.println("Contas:");
+        contaRepository.findAll().forEach(System.out::println);
+
+        System.out.println("Transferências:");
+        transferenciaRepository.findAll().forEach(System.out::println);
     }
-
 }
