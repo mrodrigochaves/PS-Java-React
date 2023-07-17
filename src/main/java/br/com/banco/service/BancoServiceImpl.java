4c96728dc3866848e7306b8f4e8ca5fa6d47c65d
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-16 22:24:49 -0300

    refatorando service implements

--- src/main/java/br/com/banco/service/BancoServiceImpl.java
+++ src/main/java/br/com/banco/service/BancoServiceImpl.java
@@ -33,51 +33,6 @@ public class BancoServiceImpl implements BancoService {
     }
 
     @Override
-    public Optional<ContaDTO> createConta(@Valid ContaDTO request) {
-        Conta conta = mapper.map(request, Conta.class);
-        Conta savedConta = contaRepository.saveAndFlush(conta);
-        return Optional.of(mapper.map(savedConta, ContaDTO.class));
-    }
-
-    @Override
-    public List<ContaDTO> getAllContas() {
-        List<Conta> contas = contaRepository.findAll();
-        return mapContasListToDTO(contas);
-    }
-
-    @Override
-    public Optional<ContaDTO> getById(Long id) {
-        return contaRepository.findById(id)
-                .map(conta -> mapper.map(conta, ContaDTO.class));
-    }
-
-    @Override
-    public Optional<ContaDTO> deleteById(Long id) {
-        Optional<Conta> conta = contaRepository.findById(id);
-        conta.ifPresent(ct -> contaRepository.deleteById(id));
-        return conta.map(ct -> mapper.map(ct, ContaDTO.class));
-    }
-
-    @Override
-    public List<ContaDTO> getByResponsavel(String responsavel) {
-        List<Conta> contas = contaRepository.findByNomeResponsavel(responsavel);
-        return mapContasListToDTO(contas);
-    }
-
-    @Override
-    public Optional<ContaDTO> update(Long id, @Valid ContaDTO request) {
-        Optional<Conta> contaOptional = contaRepository.findById(id);
-        if (contaOptional.isPresent()) {
-            Conta conta = contaOptional.get();
-            conta.setNomeResponsavel(request.getNomeResponsavel());
-
-            contaRepository.save(conta);
-            return Optional.of(mapper.map(conta, ContaDTO.class));
-        }
-        return Optional.empty();
-    }
-
-    @Override
     public Optional<TransferenciaDTO> createTransferencia(@Valid TransferenciaDTO request) {
         Transferencia transferencia = mapper.map(request, Transferencia.class);
         Transferencia savedTransferencia = transferenciaRepository.saveAndFlush(transferencia);
@@ -91,44 +46,16 @@ public class BancoServiceImpl implements BancoService {
     }
 
     @Override
-    public Optional<TransferenciaDTO> getTransferenciaById(Long id) {
-        return transferenciaRepository.findById(id)
-                .map(transferencia -> mapper.map(transferencia, TransferenciaDTO.class));
+    public Optional<ContaDTO> createConta(@Valid ContaDTO request) {
+        Conta conta = mapper.map(request, Conta.class);
+        Conta savedConta = contaRepository.saveAndFlush(conta);
+        return Optional.of(mapper.map(savedConta, ContaDTO.class));
     }
 
     @Override
-    public Optional<TransferenciaDTO> deleteTransferenciaById(Long id) {
-        Optional<Transferencia> transferencia = transferenciaRepository.findById(id);
-        transferencia.ifPresent(tf -> transferenciaRepository.deleteById(id));
-        return transferencia.map(tf -> mapper.map(tf, TransferenciaDTO.class));
-    }
-
-    @Override
-    public List<TransferenciaDTO> getTransferenciasByTipo(String tipo) {
-        List<Transferencia> transferencias = transferenciaRepository.findByTipo(tipo);
-        return mapTransferenciasListToDTO(transferencias);
-    }
-
-    @Override
-    public List<TransferenciaDTO> getTransferenciasByConta(Long contaId) {
-        List<Transferencia> transferencias = transferenciaRepository.findByContaId(contaId);
-        return mapTransferenciasListToDTO(transferencias);
-    }
-
-    @Override
-    public Optional<TransferenciaDTO> updateTransferencia(Long id, @Valid TransferenciaDTO request) {
-        Optional<Transferencia> transferenciaOptional = transferenciaRepository.findById(id);
-        if (transferenciaOptional.isPresent()) {
-            Transferencia transferencia = transferenciaOptional.get();
-            transferencia.setDataTransferencia(request.getDataTransferencia());
-            transferencia.setValor(request.getValor());
-            transferencia.setTipo(request.getTipo());
-            transferencia.setNomeOperadorTransacao(request.getNomeOperadorTransacao());
-
-            transferenciaRepository.save(transferencia);
-            return Optional.of(mapper.map(transferencia, TransferenciaDTO.class));
-        }
-        return Optional.empty();
+    public List<ContaDTO> getAllContas() {
+        List<Conta> contas = contaRepository.findAll();
+        return mapContasListToDTO(contas);
     }
 
     private List<ContaDTO> mapContasListToDTO(List<Conta> contas) {
